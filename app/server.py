bc070c5956a2e37e67d8c7e7ecc46e42d39beb2c
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2019-11-17 03:17:13 -0500

    test

--- app/server.py
+++ app/server.py
@@ -62,7 +62,7 @@ tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
-
+#
 @app.route('/')
 async def homepage(request):
     html_file = path / 'view' / 'index.html'
